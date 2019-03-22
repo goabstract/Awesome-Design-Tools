acedbc72b7136fc3985c874ed5f51c0839c8aeec
vl <berezovsky@ex.ua>
2019-03-22 18:39:12 +0200

    add analytics

--- index.js
+++ index.js
@@ -137,6 +137,27 @@ const editHead = ({ document }, title, themeColor) => {
 			${normalizeCss}
 			${mainCss}
 		</style>
+		<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-5XZVN66');</script>
+		<!-- End Google Tag Manager -->
+
+		<!-- Facebook Pixel Code -->
+		<script>
+				!function(f,b,e,v,n,t,s)
+				{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+				n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+				if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+				n.queue=[];t=b.createElement(e);t.async=!0;
+				t.src=v;s=b.getElementsByTagName(e)[0];
+				s.parentNode.insertBefore(t,s)}(window, document,'script',
+				'https://connect.facebook.net/en_US/fbevents.js');
+				fbq('init', '1844132335637828');
+				fbq('track', 'PageView');
+		</script>
 	`;
 }
 

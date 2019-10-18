7ac3909e1a5ba86659c2642bd18086bfe0aaccd0
Vladimir Berezovsky <berezovsky@ex.ua>
2019-10-18 14:45:59 +0300

    change md file for parsing

--- docs/modules/config/plugins.js
+++ docs/modules/config/plugins.js
@@ -15,7 +15,7 @@ const {
 } = require('../favicon-adp'); 
 
 const CONFIG = {
-	markdownFile: `./plugins.md`,
+	markdownFile: `./Awesome-Design-Plugins.md`,
     index: `./docs/index-plugins.html`,
     promoFlawlessFeedbackBanner__title: 'Leave feedback on iOS apps',
     promoFlawlessFeedbackBanner__description: 'Turn UI issues & Bugs Into Jira tickets or Trello cards', 

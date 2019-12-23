2379e2100fd35f138a6beb5e552174dba5fee17b
Vladimir Berezovsky <berezovsky@ex.ua>
2019-12-23 14:21:39 +0200

    new tools conf file and updated nav

--- docs/modules/config/tools.js
+++ docs/modules/config/tools.js
@@ -12,27 +12,27 @@ const {
     favicon57,
     favicon32,
     favicon16,
-} = require('../favicon-adp'); 
+} = require('../image'); 
 
 const CONFIG = {
-	markdownFile: `./Awesome-Design-Plugins.md`,
-    index: `./docs/index-plugins.html`,
+	markdownFile: `./README.md`,
+	index: `./docs/index.html`,
     promoFlawlessFeedbackBanner__title: 'Leave feedback on iOS apps',
     promoFlawlessFeedbackBanner__description: 'Turn UI issues & Bugs Into Jira tickets or Trello cards', 
     promoFlawlessFeedbackBanner__link: 'https://flawlessapp.io/feedback',
     main: {
-        title: 'Awesome Design Plugins',
-        themeColor: '#F2FAF5',
-        bodyColorScheme: '-green',
-        logoClassName: '-logo-adp',
+        title: 'Awesome Design Tools',
+        themeColor: '#0054d7',
+        bodyColorScheme: '-blue',
+        logoClassName: '-logo-adt',
         head: {
-            title: 'Awesome Design Plugins',
+            title: 'Awesome Design Tools',
             meta: {
-                title: 'Awesome Design Plugins',
-                themeColor: '#F2FAF5',
-                description: 'Design plugins',
+                title: 'Awesome Design Tools',
+                themeColor: '#0054d7',
+                description: 'The best design tools for everything — from wireframing and prototyping to animation, accessibility, AR, design systems and sound design.',
                 image: 'https://flawlessapp.io/images/designtools/awesome-design-tools-sharable-image-general.png',
-                siteURL: 'https://flawlessapp.io/designplugins',
+                siteURL: 'https://flawlessapp.io/designtools',
             },
             favicon: {
                 '196': favicon196,
@@ -55,33 +55,26 @@ const CONFIG = {
                 {
                     title: 'Tools',
                     href: '/designtools',
-                    isActive: false,
+                    isActive: true,
                     className: '-tool',
                 },
                 {
                     title: 'Plugins',
                     href: '/designplugins',
-                    isActive: true,
-                    className: '-plugin',
-                },
-                {
-                    title: 'UI Kits',
-                    href: '/designkits',
                     isActive: false,
-                    className: '-kit',
+                    className: '-plugin',
                 },
             ],
         },
         welcomeArticle: {
-            title: 'Awesome Design Plugins',
-            logoClassName: '-logo-adp',
-            description: `All the best design plugins for Sketch, Adobe XD, and Figma, gathered in one place.`,
+            title: 'Awesome Design Tools',
+            logoClassName: '-logo-adt',
+            description: `The best design tools for everything. Curated by Lisa Dziuba & Valia Havruliyk from <a href="https://flawlessapp.io/feedback">Flawless team</a>.`,
             button: {
-                href: 'https://github.com/LisaDziuba/Awesome-Design-Tools/blob/master/Awesome-Design-Plugins.md',
+                href: 'https://github.com/LisaDziuba/Awesome-Design-Tools',
             },
         },
-
     },
-};
+}
 
 module.exports = CONFIG;
